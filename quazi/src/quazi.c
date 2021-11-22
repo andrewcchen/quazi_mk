582ece690944bb82a440c7e5c73ea067084bba7e
Andrew Chen <andrew@xortux.com>
2021-11-22 22:53:47 +0000

    main loop changes

--- quazi/src/quazi.c
+++ quazi/src/quazi.c
@@ -18,7 +18,35 @@
 #include "qmk_glue.h"
 #include "quazi.h"
 
+#include <pm/state.h>
+#include <pm/pm.h>
+
 LOG_MODULE_REGISTER(quazi, CONFIG_QUAZI_LOG_LEVEL);
+
+static struct k_timer main_timer;
+static struct k_work main_work;
+
+static void main_timer_handler(struct k_timer *)
+{
+	k_work_submit(&main_work);
+}
+
+static void quazi_main_task(struct k_work *)
+{
+	quazi_qmk_task();
+}
+
+void quazi_main_loop_start(void)
+{
+	LOG_INF("main loop start");
+	k_timer_start(&main_timer, K_NO_WAIT, K_MSEC(10));
+}
+
+void quazi_main_loop_stop(void)
+{
+	LOG_INF("main loop stop");
+	k_timer_stop(&main_timer);
+}
 
 void quazi_main(void)
 {
@@ -34,11 +62,10 @@ void quazi_main(void)
 	quazi_profile_init();
 	quazi_qmk_init();
 
+	k_timer_init(&main_timer, main_timer_handler, NULL);
+	k_work_init(&main_work, quazi_main_task);
+
 	LOG_DBG("init done");
 
-	while (1) {
-		quazi_qmk_task();
-
-		k_sleep(K_MSEC(10));
-	}
+	quazi_main_loop_start();
 }
